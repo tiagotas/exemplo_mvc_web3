8092301f6dbb2c73e819992041c7be151b990a28
Aluno <aluno@etec.sp.gov.br>
2022-03-15 14:20:54 -0300

    adição dos comentários

--- Controller/PessoaController.php
+++ Controller/PessoaController.php
@@ -1,20 +1,25 @@
 <?php
 
 /**
- * Classe para determinar quais rotas...
+ * Classes Controller são responsáveis por processar as requisições do usuário.
+ * Isso significa que toda vez que um usuário chama uma rota, um método (função)
+ * de uma classe Controller é chamado.
+ * O método poderá devolver uma View (fazendo um include), acessar uma Model (para
+ * buscar algo no banco de dados), redirecionar o usuário de rota, ou mesmo,
+ * chamar outra Controller.
  */
 class PessoaController 
 {
     /**
-     * 
+     * Os métodos index serão usados para devolver uma View.
      */
     public static function index() 
     {
         include 'View/modules/Pessoa/ListaPessoas.php';
     }
 
-    /**
-     * 
+   /**
+     * Devolve uma View contendo um formulário para o usuário.
      */
     public static function form()
     {
@@ -22,12 +27,14 @@ class PessoaController
     }
 
     /**
-     * 
+     * Preenche um Model para que seja enviado ao banco de dados para salvar.
      */
     public static function save() {
 
-        include 'Model/PessoaModel.php';
+        include 'Model/PessoaModel.php'; // inclusão do arquivo model.
 
+        // Abaixo cada propriedade do objeto sendo abastecida com os dados informados
+        // pelo usuário no formulário (note o envio via POST)
         $pessoa = new PessoaModel();
         $pessoa->nome = $_POST['nome'];
         $pessoa->rg = $_POST['rg'];
@@ -37,8 +44,8 @@ class PessoaController
         $pessoa->telefone = $_POST['telefone'];
         $pessoa->endereco = $_POST['endereco'];
 
-        $pessoa->save();
+        $pessoa->save();  // chamando o método save da model.
 
-        echo 'Registro inserido com sucesso!';
+        header("Location: /pessoa"); // redirecionando o usuário para outra rota.
     }
 }
